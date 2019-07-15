994b6cbc6cb1e3148f08cf4eadd701bb95c6bec3
Kate Monahan <katemona514@gmail.com>
2019-07-15 11:25:40 -0500

    i0 Complete

---
+++ ToDo.swift
@@ -0,0 +1,16 @@
+//
+//  ToDo.swift
+//  todo
+//
+//  Created by Apple on 7/15/19.
+//  Copyright © 2019 KWK. All rights reserved.
+//
+
+import UIKit
+
+class ToDo {
+    var name = ""
+    var important = false
+    init(todoname : String){
+    }
+}
